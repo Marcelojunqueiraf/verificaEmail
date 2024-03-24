c32c4ddf2b6c3d0325b29047b93fbea4ac755c99
Marcelo Junqueira <marcelojunqueiraf@gmail.com>
2024-03-24 19:20:40 -0300

    fix enviromnent variable problems

--- app/api/verify-code/route.ts
+++ app/api/verify-code/route.ts
@@ -18,8 +18,8 @@ export async function POST(request: Request) {
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
     credentials: {
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      client_id: process.env.GOOGLE_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_CLIENT_EMAIL,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     },
   });
 
--- app/api/verify/route.ts
+++ app/api/verify/route.ts
@@ -21,7 +21,7 @@ export async function POST(request: Request) {
     credentials: {
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
       client_id: process.env.GOOGLE_CLIENT_ID,
-      private_key: process.env.GOOGLE_PRIVATE_KEY,
+      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     },
   });
 
